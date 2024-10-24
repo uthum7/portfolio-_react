c55f5814b018b04e1df0b6cb8471f715afd1a654
uthumWijenayake <uthumwijenayake@gmail.com>
2024-10-24 14:34:15 +0530

    about page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import About from './Components/About'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <About/>
     </div>
   )
 }
