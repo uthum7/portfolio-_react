bdaac64a0735927de3eaf11b54532ecba2f94289
uthumWijenayake <uthumwijenayake@gmail.com>
2024-10-25 08:54:21 +0530

    services page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import About from './Components/About'
+import Service from './Components/Service'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
+      <Service/>
     </div>
   )
 }
---
+++ src/Components/Service.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Service.css';
+import theme_pattern from '../assets/theme_pattern.svg';
+import Services_Data from '../assets/services_data'
+import arrow_icon from '../assets/arrow_icon.svg';
+
+const Service = () => {
+  return (
+    <div className='services'>
+      <div className="services-title">
+        <h1>My Services</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="services-container">
+           {Services_Data.map((Service,index)=>{
+            return <div key={index} className='services-format'>
+                <h3>{Services_Data.s_no}</h3>
+                <h2>{Service.s_name}</h2>
+                <p>{Service.s_desc}</p>
+                <div className="services-readmore">
+                    <p>Read More</p>
+                    <img src={arrow_icon} alt="" />
+                </div>
+            </div>
+           })}
+      </div>
+    </div>
+  )
+}
+
+export default Service
